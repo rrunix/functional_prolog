385d83528328395567693c4aec2f63c190d67fe5
rrunix <ruben.rrf93@gmail.com>
2017-12-30 14:45:13 +0100

    modified to add compatibility with ciao prolog implementation

--- lang.pl
+++ lang.pl
@@ -1,34 +1,67 @@
+:- use_module(library(write)).
 :- op( 500, fx, if).
 :- op( 600, xfy, then).
 :- op( 600, xfx, else).
 
-% Execute the expresion Exp of the clauses in File.
+% exec(Exp, File): Execute the expresion Exp of the clauses in File.
 exec(Exp,File):-
-   consult(File),
+   read_func_lang(File),
    eval(Exp, R),
    write(Exp),
    write('='),
-   write(R).
+   write(R),
+   write('\n').
 
-% Convert functional file to prolog
-term_expansion(DEF is Body , Clause) :-
+% read_func_lang(File): Load into prolog the functional predicates in
+% the file File.
+read_func_lang(File) :-
+   atom_concat(File, '.pl', FullFile),
+   open(FullFile, read, S),
+   read_func_lang_stream(S).
+
+% read_func_lang_stream(S): Load into prolog the funcional predicates in
+% the stream S.
+read_func_lang_stream(S) :-
+   read(S, T),
+   (T \== end_of_file ->
+       def(T),
+       read_func_lang_stream(S)
+   ; true).
+
+% def(X): Load the functional predicate X into prolog.
+def(Def is Body) :-
+   def(Def, Body), !.
+
+def(X) :-
+   write(["Warning: Ignoring malformed predicate; Functional Predicates are defined 'Def is Body'", X]).
+
+% def(X): Load the functional predicate Def is Body into prolog.
+def(Def, Body) :-
+   expand_term(Def is Body, Clause),
+   assertz(Clause).
+
+% expand_term(X, Clause). Clause is the result of translating X from our
+% functional language.
+expand_term(DEF is Body , Clause) :-
 	DEF =.. Terms,
         append(Terms, [__OUT], NewTerms),
         NewDef =.. NewTerms,
         Clause = :-(NewDef, eval(Body, __OUT)).
 
-
+% eval(Exp, Val): Val is the result of evaluating the expresion Exp.
+%
 % Evaluate primitive-datatypes.
 eval(Exp, Val) :- number(Exp), !, Val is Exp.
 
 % If structure, works with both fail or 0 bool.
 eval(if C then X else Y, Val) :-  !, ((eval(C, R), R =\= 0) -> eval(X, Val); eval(Y, Val)) .
 
-% List-like structure
-eval([], []) :- !.
+% List-like structure,
+% Note: this predicate rely on an extern predicate to perform the
+% evaluation to increase the performance, but would be possible to
+% achieve using a recursive call over eval.
 eval([X|Xs], [Y|Ys]) :-
-   eval(X, Y),
-   eval(Xs, Ys), !.
+   eval_list([X|Xs], [Y|Ys]), !.
 
 % Evaluate arithmetical operations
 eval(Exp, Val) :-
@@ -51,29 +84,54 @@ eval(Exp, Val) :-
 % as the ouput is the last argument.
 eval(Exp, Val) :-
    Exp =.. [Func | Pargs],
-   current_predicate(Func, _),
+   exists_predicate(Func, _),
    !,
    eval(Pargs, Args),
    append(Args, [Val], NewArgs),
-   apply(Func, NewArgs).
+   apply_call(Func, NewArgs).
 
 % Not matching function found, raising exception.
 eval(Exp, _):-
-   throw(["Unrecognized Expresion", Exp]).
+   throw([unrecognized_expression, Exp]).
 
-% Operator X is arithmetic
+% eval_list(X, Y): Y is the resulting list of evaluating the
+% individual values of X.
+eval_list([], []).
+eval_list([X|Xs], [Y|Ys]) :-
+   eval(X, Y),
+   eval_list(Xs, Ys).
+
+% exists_predicate(Pred, Args): Predicate Pred with arity
+% length(Args) is defined.
+exists_predicate(Pred, Args) :-
+   length(Args, X),
+   current_predicate(Pred/X).
+
+% arithmetic_op(X): X is an arithmetical operator
 arithmetic_op(X) :-
-   X = +; X = -; X = *; X = /; X = mod.
+   X = +,   !;
+   X = -,   !;
+   X = *,   !;
+   X = /,   !;
+   X = mod, !.
 
-% Operator X is comparator
+% comparator_op(X): X is a comparator operator.
 comparator_op(X) :-
-   X = >; X = <; X = >=; X = <=; X = =:=; X = =\= .
+   X = >,   !;
+   X = <,   !;
+   X = >=,  !;
+   X = <=,  !;
+   X = =:=, !;
+   X = =\=, !.
 
-% X is the result of applyng Operator Op to arguments Args. 1 means
-% true, whereas 0 false.
-func_op(Op, Args, X) :- (apply(Op, Args) -> X is 1; X is 0), !.
+% func_op(Op, Args, X) : X is the boolean result (0 if fail, 1 if
+% succed) of evaluating the expresion Func(Args).
+func_op(Op, Args, X) :- (apply_call(Op, Args) -> X is 1; X is 0).
 
-
+% apply_call(Func, Args): Call predicate Func(Args).
+apply_call(Func, Args) :-
+   Expr =.. [Func | Args],
+   call(Expr).
 
 
 
